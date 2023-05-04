dbffcc543d9695775182736bf5bab220f52387bc
Gonzalez Allende <hugoglez@amazon.es>
2023-05-04 20:29:54 +0200

    A ver ahora

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { AmplifyProvider, Authenticator } from "@aws-amplify/ui-react";
+import { StrictMode } from "react";
+import ReactDOM from "react-dom";
 
+import App from "./App";
+
+const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <StrictMode>
+    <AmplifyProvider>
+      <Authenticator.Provider>
+        <App />
+      </Authenticator.Provider>
+    </AmplifyProvider>
+  </StrictMode>,
+  rootElement
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
